bd55c0dbd92de836499f7aed4d38682d01cd1041
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2019-03-06 13:15:52 -0800

    Initial commit

---
+++ quotes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+app.use(bodyParser.text({type: "*/*"}));
+
+app.post("/", function(req, res) {
+
+    var text = "";
+    var author = "";
+    request('https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', { json: true }, (err, response, body) => {
+        if (err) { return console.log(err); }
+        console.log(body.quoteText);
+        console.log(body.quoteAuthor);
+        text = body.quoteText;
+        author = body.quoteAuthor;
+        res.send(text + "  -" + author);
+    });
+
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log("Server started on port", port);
+});
